7b344e1d3f210bab988f5a84e25d7ced2f8272a8
Boinitchi_Ion <boinitchi.ion@gmail.com>
2018-11-19 00:23:00 +0200

    fix version of symfony - Implement faker plugin - Change DataFixtures class

--- src/DataFixtures/ApplicationFixtures.php
+++ src/DataFixtures/ApplicationFixtures.php
@@ -5,11 +5,14 @@ namespace App\DataFixtures;
 use App\Entity\Actor;
 use App\Entity\Film;
 use App\Entity\FilmStudio;
+use Faker;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class ApplicationFixtures extends Fixture
 {
+    private $faker;
+
     private $dateGanre = [
         'Action',
         'Adventure',
@@ -19,29 +22,22 @@ class ApplicationFixtures extends Fixture
         'Crime'
     ];
 
-    private $dateDescription = [
-        'Lorem ipsum dolor sit amet, an pro essent delenit, altera atomorum vix ne. 
-        Cum congue iudico verear ad, vel utamur posidonium no. Cu quo natum dicunt, 
-        expetenda deseruisse cu quo. Ut duo soluta epicurei ocurreret, ea usu ferri 
-        tollit feugiat, ei pro torquatos posidonium eloquentiam. Causae malorum nam 
-        cu, amet dignissim his ne, utinam everti te sed.',
-        'Ei has mutat delenit eloquentiam, quo magna euismod an, oblique vituperatoribus 
-        an pri. Eu urbanitas voluptaria scripserit qui, qui deleniti quaestio cu. Eos in 
-        partiendo aliquando. Ex vim dicta cotidieque dissentiunt. Eu dico dolore 
-        petentium ius, sed quot volutpat te.',
-        'Ex iisque eloquentiam sea, vide dicant utinam ut nam. An eos viderer nusquam. 
-        Volumus scaevola ocurreret cum eu, nam an erat justo. Ut tale meis quo, te 
-        quod tibique euripidis eum. Cum ut velit possim adversarium.'
-    ];
+    private $dateDescription = [];
 
-    private $dateAddress = [
-        '9 Pawnee Dr. Saint Charles, IL 60174',
-        '9422 4th Street Greenville, NC 27834',
-        '217 Fulton Rd. Voorhees, NJ 08043',
-        '94 Highland Ave. The Villages, FL 32162',
-        '7939 W. Rocky River Drive Mahwah, NJ 07430',
-        '619 Euclid Drive Waterford, MI 48329'
-    ];
+    private $dateAddress = [];
+
+    public function __construct()
+    {
+        // Can use facker populator but we use relation
+        // $generator = \Faker\Factory::create();
+        // $populator = new Faker\ORM\Propel\Populator($generator);
+
+        $this->faker = Faker\Factory::create();
+        for ($i = 0; $i < 10; $i++) {
+            $this->dateAddress[] = $this->faker->address;
+            $this->dateDescription[] = $this->faker->realText(500, 2);
+        }
+    }
 
     public function getRandomGanre(){
         return $this->dateGanre[rand(0, count($this->dateGanre) - 1)];
@@ -52,7 +48,7 @@ class ApplicationFixtures extends Fixture
     }
 
     public function getRandomPhone(){
-        return rand(1111111111, 9999999999);
+        return $this->faker->tollFreePhoneNumber;
     }
 
     public function getRandomAddress(){
@@ -64,11 +60,11 @@ class ApplicationFixtures extends Fixture
      */
     public function load(ObjectManager $manager)
     {
-        $count = 10;
+        $count = 15;
         $actors = $studios = $films = [];
         for ($s = 0; $s < $count; $s++) {
             $studio = new FilmStudio();
-            $studio->setName("Studio Name_{$s}");
+            $studio->setName($this->faker->company);
             $studio->setPhone($this->getRandomPhone());
             $studio->setAddress($this->getRandomAddress());
             $studio->setDateCreate(new \DateTime("now"));
@@ -78,8 +74,8 @@ class ApplicationFixtures extends Fixture
 
         for ($a = 0; $a < $count; $a++) {
             $actor = new Actor();
-            $actor->setName("Name Actor{$a}");
-            $actor->setEmail("user{$a}.actor@email.com");
+            $actor->setName($this->faker->name);
+            $actor->setEmail($this->faker->email);
             $actor->setPhone($this->getRandomPhone());
             $actors[] = $actor;
             $manager->persist($actor);
@@ -87,7 +83,7 @@ class ApplicationFixtures extends Fixture
 
         for ($f = 0; $f < $count; $f++) {
             $film = new Film();
-            $film->setName("Film {$f}");
+            $film->setName($this->faker->realText(rand(10, 20),2));
             $film->setGenre($this->getRandomGanre());
             $film->setDescription($this->getRandomDescription());
             $film->setDateCreate(new \DateTime("now"));
--- src/Repository/FilmStudioRepository.php
+++ src/Repository/FilmStudioRepository.php
@@ -18,33 +18,4 @@ class FilmStudioRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, FilmStudio::class);
     }
-
-//    /**
-//     * @return FilmStudio[] Returns an array of FilmStudio objects
-//     */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('f')
-            ->andWhere('f.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('f.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?FilmStudio
-    {
-        return $this->createQueryBuilder('f')
-            ->andWhere('f.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
